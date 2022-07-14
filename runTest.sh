b84ce8efbf4c03c84b66cfaed7008360d0074db9
Wheelinventor <info@luohuizhe.com>
2022-07-14 13:44:57 +1000

    uodate path

--- runTest.sh
+++ runTest.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "--- Generate job yaml ---"
 
-( echo "cat <<EOF" ; cat ./env.json.tmpl; echo EOF ) | \
+( echo "cat <<EOF" ; cat env.json.tmpl; echo EOF ) | \
 sh > env.json
 
-cat ./env.json
+cat env.json
 
 echo "--- start ---"
 
 echo "--- put 6 messages ---"
-java -jar ./app.jar put 6
+java -jar app.jar put 6
 
 echo "--- get messages ---"
-java -jar ./app.jar get
+java -jar app.jar get
 
 echo "--- done ---"
